4d751187c42a9275e329ea62312f2463b73f5fc9
Aperçu <bgronon@gmail.com>
2016-04-16 15:01:10 +0200

    refactor(lint): switch to eslint

--- test/index.js
+++ test/index.js
@@ -1,25 +1,22 @@
+'use strict';
 
 var Test = require('segmentio-integration-tester');
 var helpers = require('./helpers');
-var facade = require('segmentio-facade');
-var mapper = require('../lib/mapper');
-var should = require('should');
-var assert = require('assert');
 var extend = require('extend');
 var TrakIO = require('..');
 
-describe('trak.io', function(){
+describe('trak.io', function() {
   var settings;
   var test;
   var tio;
 
-  beforeEach(function(){
+  beforeEach(function() {
     settings = { token: '740d36a79fb593bbc034a3ac934bc04f5a591c0c' };
     tio = new TrakIO(settings);
     test = Test(tio, __dirname);
   });
 
-  it('should have the correct settings', function(){
+  it('should have the correct settings', function() {
     test
       .name('trak.io')
       .endpoint('https://api.trak.io/v1')
@@ -28,63 +25,63 @@ describe('trak.io', function(){
       .retries(2);
   });
 
-  describe('.validate()', function () {
-    it('should be invalid if .token is missing', function(){
+  describe('.validate()', function() {
+    it('should be invalid if .token is missing', function() {
       delete settings.token;
       test.invalid({}, settings);
     });
 
-    it('should be valid if settings are complete', function(){
+    it('should be valid if settings are complete', function() {
       test.valid({}, settings);
     });
   });
 
-  describe('mapper', function(){
-    describe('identify', function(){
-      it('should map basic identify', function(){
+  describe('mapper', function() {
+    describe('identify', function() {
+      it('should map basic identify', function() {
         test.maps('identify-basic');
       });
 
-      it('should map anonymous identify', function(){
+      it('should map anonymous identify', function() {
         test.maps('identify-anonymous');
       });
     });
 
-    describe('track', function(){
-      it('should map basic track', function(){
+    describe('track', function() {
+      it('should map basic track', function() {
         test.maps('track-basic');
       });
 
-      it('should map anonymous track', function(){
+      it('should map anonymous track', function() {
         test.maps('track-anonymous');
       });
     });
 
-    describe('alias', function(){
-      it('should map basic alias', function(){
+    describe('alias', function() {
+      it('should map basic alias', function() {
         test.maps('alias-basic');
       });
     });
 
-    describe('company', function(){
-      it('should map basic company', function(){
+    describe('company', function() {
+      it('should map basic company', function() {
         test.maps('company-basic');
       });
 
-      it('should map company with anonymous identity', function(){
+      it('should map company with anonymous identity', function() {
         test.maps('company-anonymous');
       });
     });
   });
 
-  describe('.track()', function () {
-    it('should get a good response from the API', function(done){
+  describe('.track()', function() {
+    it('should get a good response from the API', function(done) {
       var track = helpers.track();
       test
         .set(settings)
         .track(track)
         .sends({
-          source: "segment",
+          source: 'segment',
           data: {
             distinct_id: track.userId(),
             event: track.event(),
@@ -96,14 +93,14 @@ describe('trak.io', function(){
     });
   });
 
-  describe('.identify()', function () {
-    it('should get a good response from the API', function(done){
+  describe('.identify()', function() {
+    it('should get a good response from the API', function(done) {
       var identify = helpers.identify();
       test
         .set(settings)
         .identify(identify)
         .sends({
-          source: "segment",
+          source: 'segment',
           data: {
             distinct_id: identify.userId(),
             properties: extend(identify.traits(), {
@@ -127,14 +124,14 @@ describe('trak.io', function(){
     });
   });
 
-  describe('.alias()', function () {
-    it('should get a good response from the API', function(done){
+  describe('.alias()', function() {
+    it('should get a good response from the API', function(done) {
       var alias = helpers.alias();
       test
         .set(settings)
         .alias(alias)
         .sends({
-          source: "segment",
+          source: 'segment',
           data: {
             distinct_id: alias.from(),
             alias: alias.to()
@@ -144,14 +141,14 @@ describe('trak.io', function(){
     });
   });
 
-  describe('.group()', function () {
-    it('should get a good response from the API', function(done){
+  describe('.group()', function() {
+    it('should get a good response from the API', function(done) {
       var group = helpers.group();
       test
         .set(settings)
         .group(group)
         .sends({
-          source: "segment",
+          source: 'segment',
           data: {
             people_distinct_ids: [group.userId()],
             company_id: group.groupId(),
